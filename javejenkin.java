1f3407e0720d931fb38c92041818ec1189149ef4
neha <por.neha@gmail.com>
2024-11-06 09:57:48 -0500

    added java file for jenkin

---
+++ javejenkin.java
@@ -0,0 +1,8 @@
+public class Demo {
+  public static void main (string [] args){
+  system.out.prinin ("hello from Demo");
+  system.out.printin ("hello to sweta from demom java");
+
+      }
+
+  }
